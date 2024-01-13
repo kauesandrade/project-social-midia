ee6286c99d24bd5434ef2fbb46505974a8f47c61
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2024-01-13 12:52:39 -0300

    doing a post

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -1,5 +1,13 @@
 import './style.css'
 
+import { FaRegHeart } from "react-icons/fa";
+// import { FaHeart } from "react-icons/fa6"; -- Para fazer o like <FaHeart />
+import { FaRegComment } from "react-icons/fa6";
+import { GoPaperAirplane } from "react-icons/go";
+import { MdBookmarkBorder } from "react-icons/md";
+// import { MdOutlineBookmark } from "react-icons/md"; --Para add no saves <MdOutlineBookmark />
+
+
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -9,6 +17,7 @@ import { Pagination } from 'swiper/modules';
 const post = () => {
     return (
         <article>
+            {/* FAZER UM COMPONENT PARA OS SLIDES */}
             <Swiper
                 pagination={{
                     dynamicBullets: true,
@@ -16,13 +25,32 @@ const post = () => {
                 modules={[Pagination]}
                 className="mySwiper"
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide><img src="src/assets/imagens/profile.jpg" alt="Profile Photo" /></SwiperSlide>
                 <SwiperSlide>Slide 2</SwiperSlide>
                 <SwiperSlide>Slide 3</SwiperSlide>
             </Swiper>
 
             <section>
-                <p>cuuuu</p>
+                <div className='divIconsPost'>
+                    <div className='divFirstIcons'>
+                        <div className='divLikeCounter'>
+                            <FaRegHeart className='iconsPost'/>  
+                            <p className='pCounter'>1000</p>
+                        </div>
+                        <FaRegComment className='iconsPost'/>
+                        <GoPaperAirplane className='iconsPost'/>
+                    </div>
+                    <div className='divSecondIcons'>
+                        <MdBookmarkBorder className='iconsPost'/>
+                    </div>
+                </div>
+                <div className='divDescriptionPost'>
+                    <p><span className='spanUsernamePost'>@TatarugaNinja</span> Leonardo sendo leonardo</p>
+                </div>
+                <div className='divCommentInputPost'>
+                    <input className='inputCommentPost' type='text' placeholder='Add a comment...'></input>
+                    <button className='buttonShareCommentPost'>Share</button>
+                </div>
             </section>
         </article>
     )
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -12,62 +12,97 @@ import { useState } from 'react';
 const sidebar = () => {
 
     const navigate = useNavigate();
-    const[isShowMenu, setIsShowMenu] = useState(false);
-    const[isShowMenuBar, setIsShowMenuBar] = useState(true);
+    const [isShowMenu, setIsShowMenu] = useState(false);
+    const [isShowMenuBar, setIsShowMenuBar] = useState(true);
 
-    const handleClickMenu = () =>{
-        // isShowMenuBar == false ? setIsShowMenuBar(true) : setIsShowMenuBar(false);
+    const handleClickMenu = () => {
+        isShowMenuBar == false ? setIsShowMenuBar(true) : setIsShowMenuBar(false);
         isShowMenu == false ? setIsShowMenu(true) : setIsShowMenu(false);
     }
 
+    // ====================FAZER UM COMPONENT PARA OS LI===========================
+
+
     return (
         <>
+            <aside className='asideSidebar'>
+                <ul className='ulNavBar'>
+                    <li className='liNavBar'>
+                        <span className='spanPhoto' onClick={() => navigate('/profile')}>
+                            <img className='imgNav' src="src/assets/imagens/profile.jpg" alt="Profile Photo" />
+                        </span>
+                    </li>
+                    <li className='liNavBar'>
+                        <span className='spanIconNav' onClick={() => navigate('/')}>
+                            <PiHouse className='iconsNav' />
+                            <a id='home'>Home</a>
+                        </span>
+                    </li>
+                    <li className='liNavBar'>
+                        <span className='spanIconNav' onClick={() => navigate('/explore')}>
+                            <GoHash className='iconsNav' />
+                            <a id='explore'>Explore</a>
+                        </span>
+                    </li>
+                    <li className='liNavBar'>
+                        <span className='spanIconNav' onClick={() => navigate('/menssages')}>
+                            <FaRegCommentAlt className='iconsNav' />
+                            <a id='menssages'>Menssages</a>
+                        </span>
+                    </li>
+                    <li className='liNavBar'>
+                        <span className='spanIconNav' onClick={() => navigate('/profile')}>
+                            <IoPersonOutline className='iconsNav' />
+                            <a id='profile'>Profile</a>
+                        </span>
+                    </li>
+                </ul>
+            </aside>
 
-        <aside className='asideSidebar'>
-            <ul>
-                <li><span className='spanPhoto' onClick={() =>navigate('/profile')}><img src="src/assets/imagens/profile.jpg" alt="Profile Photo" /></span></li>
-                <li><span onClick={() =>navigate('/')}><PiHouse style={{fontSize: '1.5rem'}} /><a id='home'>Home</a></span></li>
-                <li><span onClick={() =>navigate('/explore')}><GoHash style={{ fontSize: '1.5rem' }} /><a id='explore'>Explore</a></span></li>
-                <li><span onClick={() =>navigate('/menssages')}><FaRegCommentAlt style={{ fontSize: '1.5rem' }} /><a id='menssages'>Menssages</a></span></li>
-                <li><span onClick={() =>navigate('/profile')}><IoPersonOutline style={{fontSize: '1.5rem'}} /><a id='profile'>Profile</a></span></li>
-            </ul>
-        </aside>
+            {isShowMenuBar && (
+                <nav className='navBarMenu'>
+                    <IoIosMenu onClick={() => handleClickMenu()} style={{ fontSize: '3rem' }} />
+                    <a className='aMenu'>Menu</a>
+                </nav>
+            )}
 
-        {isShowMenuBar && (
-            <nav className='navBarMenu'>
-            <IoIosMenu onClick={() =>handleClickMenu()} style={{ fontSize: '3rem' }} />
-            <a className='aMenu'>Menu</a>
-        </nav>
-        )}
-        
-        
-
-        {isShowMenu && (
-        <nav className='navBarShow'>
-            <ul>
-                <li><span onClick={() =>handleClickMenu()}><IoClose style={{fontSize: '2.5rem'}} /></span></li>
-                <li><span onClick={() =>{handleClickMenu(); navigate('/')}}><PiHouse style={{fontSize: '1.5rem'}} /><a id='home'>Home</a></span></li>
-                <li><span onClick={() =>{handleClickMenu(); navigate('/explore')}}><GoHash style={{ fontSize: '1.5rem' }} /><a id='explore'>Explore</a></span></li>
-                <li><span onClick={() =>{handleClickMenu(); navigate('/menssages')}}><FaRegCommentAlt style={{ fontSize: '1.5rem' }} /><a id='menssages'>Menssages</a></span></li>
-                <li><span onClick={() =>{handleClickMenu(); navigate('/profile')}}><IoPersonOutline style={{fontSize: '1.5rem'}} /><a id='profile'>Profile</a></span></li>
-            </ul>
-
-            {/* <span onClick={() =>handleClickMenu()}><IoClose style={{fontSize: '1.5rem'}} /></span>
-            <span onClick={() =>{handleClickMenu(); navigate('/')}}><FaHouse style={{fontSize: '1.5rem'}} /></span>
-            <span onClick={() =>{handleClickMenu(); navigate('/explore')}}><FaHashtag style={{ fontSize: '1.5rem' }} /></span>
-            <span onClick={() =>{handleClickMenu(); navigate('/menssages')}}><FaCommentAlt style={{ fontSize: '1.5rem' }} /></span>
-            <span onClick={() =>{handleClickMenu(); navigate('/profile')}}><IoPersonSharp style={{fontSize: '1.5rem'}} /></span> */}
-        </nav>
-        )}
+            {isShowMenu && (
+                <nav className='navBarShow'>
+                    <ul className='ulNavBar'>
+                        <li className='liNavBar'>
+                            <span className='spanIconNav' onClick={() => handleClickMenu()}>
+                                <IoClose className='iconClose' />
+                            </span>
+                        </li>
+                        <li className='liNavBar'>
+                            <span className='spanIconNav' onClick={() => { handleClickMenu(); navigate('/') }}>
+                                <PiHouse className='iconsNav' />
+                                <a id='home'>Home</a>
+                            </span>
+                        </li>
+                        <li className='liNavBar'>
+                            <span className='spanIconNav' onClick={() => { handleClickMenu(); navigate('/explore') }}>
+                                <GoHash className='iconsNav' />
+                                <a id='explore'>Explore</a>
+                            </span>
+                        </li>
+                        <li className='liNavBar'>
+                            <span className='spanIconNav' onClick={() => { handleClickMenu(); navigate('/menssages') }}>
+                                <FaRegCommentAlt className='iconsNav' />
+                                <a id='menssages'>Menssages</a>
+                            </span>
+                        </li>
+                        <li className='liNavBar'>
+                            <span className='spanIconNav' onClick={() => { handleClickMenu(); navigate('/profile') }}>
+                                <IoPersonOutline className='iconsNav' />
+                                <a id='profile'>Profile</a>
+                            </span>
+                        </li>
+                    </ul>
+                </nav>
+            )}
         </>
     )
-
-
-
-
-
-
-
 }
 
 export default sidebar;
